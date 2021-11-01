fe282de3e14383a3478934a2d4b113451bdd4ffa
h602509 <89194252+h602509@users.noreply.github.com>
2021-11-01 19:46:32 +0100

    B1 d)
    B1 d) ferdig la inn 3 frosøk før programmet terminerer.

--- programmering/jplab10/startkode/src/no/hvl/dat100/oppgave2/LeseFraFil.java
+++ programmering/jplab10/startkode/src/no/hvl/dat100/oppgave2/LeseFraFil.java
@@ -13,31 +13,34 @@ public class LeseFraFil {
 	static public void main(String[] args) throws FileNotFoundException {
 
 		String filnavn = null;
+		int i = 0;
+		while (i < 3) {
 
-		try {
+			try {
 
-			filnavn = JOptionPane.showInputDialog("Filnavn i mappen " + MAPPE_STR);
+				filnavn = JOptionPane.showInputDialog("Filnavn i mappen " + MAPPE_STR);
 
-			File file = new File(MAPPE_STR + filnavn);
-			Scanner reader = new Scanner(file);
+				File file = new File(MAPPE_STR + filnavn);
+				Scanner reader = new Scanner(file);
 
-			int linenumber = 1;
+				int linenumber = 1;
 
-			// les innhold i filen linje for linje
-			String line;
+				// les innhold i filen linje for linje
+				String line;
 
-			while (reader.hasNextLine()) {
-				line = reader.nextLine();
-				System.out.println(linenumber + " " + line);
-				linenumber++;
+				while (reader.hasNextLine()) {
+					line = reader.nextLine();
+					System.out.println(linenumber + " " + line);
+					linenumber++;
+				}
+
+				reader.close();
+			} catch (FileNotFoundException e) {
+				if (filnavn != null) {
+					JOptionPane.showMessageDialog(null, "Filen " + filnavn + " finnes ikke. \n" + e.getMessage());
+				}
 			}
-
-			reader.close();
-		} catch (FileNotFoundException e) {
-			if (filnavn != null) {
-				JOptionPane.showMessageDialog(null, "Filen " + filnavn + " finnes ikke. \n" + e.getMessage());
-			}
+			i++;
 		}
-
 	}
 }
